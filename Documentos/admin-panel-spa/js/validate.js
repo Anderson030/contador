b7c45fd06f358fba457ffbf223f6f0d4ea5f2bbb
Anderson Blandon <anderson.blandon@hotmail.com>
2025-07-11 10:39:04 -0500

    Primera versión del formulario interactivo

---
+++ Documentos/admin-panel-spa/js/validate.js
@@ -0,0 +1,5 @@
+export function validateForm(user) {
+    const { name, email, phone, enrollNumber, dateOfAdmission } = user;
+    return name && email && phone && enrollNumber && dateOfAdmission;
+  }
+  
